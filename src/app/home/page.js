6facebfd6e12da390cfedf455e0db918e0702677
Ra-Jay <rjyansonx@gmail.com>
2023-08-30 22:46:26 +0800

    Update

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -2,7 +2,13 @@ import React from "react";
 
 function Home() {
   return(
-    <div>Home</div>
+    <div className="w-full flex items-center h-screen p-3">
+      <div>
+        <p>Hello, I'm a web developer, </p>
+        <h1 className="text-green-500 text-8xl">RJ YANSON</h1>
+        <p>Code. Create. Captivate.</p>
+      </div>
+    </div>
   ) 
     
 }
